75e6ea203a8fe2544590b8797c9316cdc81b641b
Chetan <chetansiddannavar07@gmail.com>
2025-09-19 18:36:30 +0530

    inngest file edited

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,37 +1,42 @@
-import { User } from "../models/usermodel.js"; 
 import { Inngest } from "inngest";
-import { connectdb } from "./db.js";
+import { connectdb, connectDB } from "./db.js";
+import { User } from "../models/user.model.js"; // Import the User model
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
 
-export const syncUser=inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async ({event})=>{
-        await connectdb()
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectdb();
 
-        const {id,email_address,frist_name,last_name,image_url}=event.data
-        const newUser={
-            clerkId:id,
-            name:`${frist_name||""} ${last_name||""}`,
-            email_address:email_address[0]?.email_address,
-            image_url:image_url,
-        }
-        await User.create(newUser)
-        // todo two more things
-    }
-)
-const deleteUserfromdb=inngest.createFunction(
-    {id:"sync-user-from-db"},
-    {event:"clerk/user.deleted"},
-    async ({event})=>{
-        await connectdb()
-        const {id}=event.data
-        await User.deleteOne({clerkId:id})
-        //todo two more things
-    }
-)
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      image: image_url,
+    };
+
+    await User.create(newUser);
+
+    
+  }
+);
+
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectdb();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+
+    
+  }
+);
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUser,deleteUserfromdb];
+export const functions = [syncUser, deleteUserFromDB];
